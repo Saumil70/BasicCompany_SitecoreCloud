70ac827f888a5eb431a5a0d176c83b0f6b01af41
Divyesh Makwana <dmakwana@toastmasters.org>
2025-02-26 16:25:10 +0530

    Code changes for PageContent sxa component

--- src/Foundation/GenericRazorHelpers/SxaModels/PageContent.cs
+++ src/Foundation/GenericRazorHelpers/SxaModels/PageContent.cs
@@ -1,9 +1,20 @@
 ﻿using GenericRazorHelpers.SxaModels.Generic;
+using Microsoft.AspNetCore.Routing;
 using Newtonsoft.Json;
 
 namespace GenericRazorHelpers.SxaModels;
 
 public class PageContent : BaseModel
 {
+    public RichTextField? RouteContent { get; set; }
+
     public RichTextField? Text { get; set; }
+
+    public RichTextField? Content
+    {
+        get
+        {
+            return Text ?? RouteContent;
+        }
+    }
 }
