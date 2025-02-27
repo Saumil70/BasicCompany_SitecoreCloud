94aa407fc44a0fe82983b59e98c41df069c01fbd
Divyesh Makwana <dmakwana@toastmasters.org>
2025-02-27 11:37:56 +0530

    Created Container, Column Splitter, Row Splitter Sxa Component

---
+++ src/Foundation/GenericRazorHelpers/SxaModels/Container.cs
@@ -0,0 +1,35 @@
+﻿using GenericRazorHelpers.SxaModels.Generic;
+using Sitecore.AspNetCore.SDK.RenderingEngine.Binding.Attributes;
+using System.Text.RegularExpressions;
+
+namespace GenericRazorHelpers.SxaModels;
+
+public partial class Container : BaseModel
+{
+    [GeneratedRegex("/mediaurl=\\\"([^\"]*)\\\"/", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex MediaUrlPattern();
+
+    public string? BackgroundImage { get; set; }
+
+    public string? BackgroundStyle
+    { 
+        get
+        {
+            if (!string.IsNullOrEmpty(BackgroundImage) && MediaUrlPattern().IsMatch(BackgroundImage))
+            {
+                string mediaUrl = MediaUrlPattern().Match(BackgroundImage).Groups[1].Value;
+                return $"background-image: url('{mediaUrl}')";
+            }
+
+            return string.Empty;
+        }
+    }
+
+    public bool OutputContainerWrapper 
+    {
+        get
+        {
+            return Styles?.Split(' ').Any(x => x == "container") == true;
+        }
+    }
+}
